56011e887af260e25d88465392aa9d50516a2ed7
Matthew Bunday <zenceph@gmail.com>
2015-06-12 15:48:43 -0400

    Add route for individual journal entry

--- router.js
+++ router.js
@@ -5,3 +5,15 @@ Router.configure({
 Router.route('/', function () {
   this.render('journal_list');
 });
+
+Router.route('/j/:_id', function() {
+  this.wait(Meteor.subscribe('journal', this.params._id));
+
+  this.render('journal_edit', {
+    data: function() {
+      var journal = Journals.findOne({_id: this.params._id});
+      Session.set("journal", journal);
+      return journal;
+    }
+  })
+})
