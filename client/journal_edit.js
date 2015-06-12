8a4853ffe14694eee33a41792acb83bfb3668ffa
Matthew Bunday <zenceph@gmail.com>
2015-06-12 19:53:41 -0400

    Drop in autocomplete textarea

--- client/journal_edit.js
+++ client/journal_edit.js
@@ -2,3 +2,19 @@ Template.journal_edit.rendered = function() {
   var journal = Session.get("journal");
   $('#journal_text').val(journal.text);
 }
+
+Template.journal_edit.helpers({
+  settings: function() {
+    return {
+      position: "bottom",
+      limit: 5,
+      rules: [{
+        token: '#',
+        replacement: '#',
+        collection: Tags,
+        field: "name",
+        template: Template.tag_preview
+      }]
+    }
+  }
+});
