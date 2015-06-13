a4c09e1e994c824a9526f2f85972fd9d5c737a18
Matthew Bunday <zenceph@gmail.com>
2015-06-12 21:01:25 -0400

    Add autosave

--- client/journal_edit.js
+++ client/journal_edit.js
@@ -4,6 +4,25 @@ Template.journal_edit.rendered = function() {
   $('#journal_text').autosize();
 }
 
+Template.journal_edit.events({
+  'keypress': function(e) {
+    var target = $(e.target);
+
+    var timer = Session.get("autosave_timer");
+    target.removeClass("saved");
+
+    if (timer !== undefined) {
+      clearTimeout(timer);
+    }
+
+    Session.set("autosave_timer", setTimeout(function() {
+      target.addClass("saved");
+      var journal = new Journal(Session.get("journal"));
+      journal.save($('#journal_text').val());
+    }, 1500));
+  }
+});
+
 Template.journal_edit.helpers({
   settings: function() {
     return {
